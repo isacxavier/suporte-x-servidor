3b4a93246121e2c54070af34f7254ad4f06468e0
isacxavier <147409518+isacxavier@users.noreply.github.com>
2025-09-15 21:51:09 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('suportex-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/send.html',
+        '/view.html',
+        '/style.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
